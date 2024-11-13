068ed19df9983238bbec10d5aa0f834f3eb69a7e
박윤환 <gkdld1004@gmail.com>
2024-11-13 11:10:14 +0900

    counter example

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,43 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [count, setCount] = useState(0);
+  const [userInput, setUserInput] = useState("");
+
+  const handleClickPlus = () => {
+    setCount(count + 1);
+  }
+
+  const handleClickMinus = () => {
+    setCount(count - 1);
+  }
+
+  const handleUserInput = (e) => {
+    setUserInput(e.target.value);
+  }
+
+  const handleEnter = (e) => {
+    if(e.key === "Enter") {
+      setUserInput("");
+      const num = Number(userInput);
+      if(Number.isInteger(num)) setCount(num);
+    }
+  };
 
   return (
     <>
+      <div>current count :: {count}</div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>count value input :: </div>
+        <input value={userInput} onChange={handleUserInput} onKeyDown={handleEnter}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <div>buttons :: </div>
+        <button onClick={handleClickPlus}>+</button>
+        <button onClick={handleClickMinus}>-</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
